7f8601aa29349be43a08218cd05337fceec3d2a7
tanmay <tanmayshinde022000@gmail.com>
2024-05-27 12:10:19 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
-import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
+import { Home } from "./pages/Home";
 export default function App() {
-  return <h1 className="text-red-700 flex-col">App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
