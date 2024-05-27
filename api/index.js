b4d5e2f35a28a06bead6546e148f8459a516e990
tanmay <tanmayshinde022000@gmail.com>
2024-05-27 17:18:47 +0530

    itsauthentication

--- api/index.js
+++ api/index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("error connecting to mongodb:", err);
+  });
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log("server listening on port 3000");
 });
